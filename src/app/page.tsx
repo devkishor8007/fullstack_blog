47597bb43b3da456699c4e8cfc04af9029d4b8da
kishorkc <kishor8007kc@gmail.com>
2023-06-27 00:00:09 +0545

    feat: register & login page with formik

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image'
+import Register from './register/page'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <p>hello</p>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 gap-4">
+    <Register />
     </main>
   )
 }
---
+++ src/app/register/page.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import Input from '@/components/input';
+import { useFormik } from 'formik';
+
+export default function Register() {
+    const formik = useFormik({
+        initialValues: {
+            username: '',
+            password: '',
+            email: '',
+        },
+        onSubmit: values => {
+        },
+    });
+    return (
+        <div className=''>
+            <form onSubmit={formik.handleSubmit}>
+                <div className='flex flex-col justify-center text-xs md:text-sm h-[450px] w-[200px] md:w-[350px] mx-auto gap-2 mb-[200px]'>
+
+                    <div>
+                        <label htmlFor="username">Username</label>
+
+                        <Input placeholder='Enter your username' id='username' name='username' type='text' onChange={formik.handleChange} value={formik.values.username} />
+                    </div>
+                    <div>
+                        <label htmlFor="email">Email Address</label>
+                        <Input placeholder='Enter your email' id='email' name='email' type='text' onChange={formik.handleChange} value={formik.values.email} />
+
+                    </div>
+                    <div><label htmlFor="password">Password</label>
+                        <Input placeholder='Enter your password' id='password' name='password' type='password' onChange={formik.handleChange} value={formik.values.password} />
+
+                    </div>
+
+                    <button className='bg-cyan-400 w-1/2 justify-center p-2 ml-auto mr-auto mt-2 rounded-2xl hover:text-white hover:bg-black' type="submit">Signup</button>
+                </div>
+            </form>
+        </div>
+    )
+}
